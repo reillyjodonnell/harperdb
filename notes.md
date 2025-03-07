541bbf4c5490f34055138cd5ba172420a00ffab5
Reilly <odonnell.reilly.j@gmail.com>
2025-03-07 13:16:40 -0500

    bare bones project

---
+++ notes.md
@@ -0,0 +1,44 @@
+```
+fetch('http://localhost:9926/User', {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ id: 1, name: 'harper' })
+})
+Promise {<pending>}
+VM31:1
+
+
+ PUT http://localhost:9926/User 500 (Internal Server Error)
+
+2025-03-07T02:53:04.151Z [http/2] [error]: Error: Invalid primary key of null
+    at Yi (/Users/reilly/.bun/install/global/node_modules/harperdb/server/threads/threadServer.js:40:149775)
+    at User._writeUpdate (/Users/reilly/.bun/install/global/node_modules/harperdb/server/threads/threadServer.js:40:126437)
+    at User.update (/Users/reilly/.bun/install/global/node_modules/harperdb/server/threads/threadServer.js:40:123962)
+    at User.put (/Users/reilly/.bun/install/global/node_modules/harperdb/server/threads/threadServer.js:40:126343)
+    at Lr.put.Jn.hasContent (/Users/reilly/.bun/install/global/node_modules/harperdb/server/threads/threadServer.js:40:107578)
+    at /Users/reilly/.bun/install/global/node_modules/harperdb/server/threads/threadServer.js:40:105000
+    at processTicksAndRejections (node:internal/process/task_queues:105:5)
+    at http (/Users/reilly/.bun/install/global/node_modules/harperdb/server/threads/threadServer.js:40:33708)
+    at authentication (/Users/reilly/.bun/install/global/node_modules/harperdb/server/threads/threadServer.js:18:6622)
+    at Server.<anonymous> (/Users/reilly/.bun/install/global/node_modules/harperdb/server/threads/threadServer.js:132:5158)
+```
+
+```
+reilly@mac harperdb-app % curl --location --request POST 'http://localhost:9926/User' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "name": "reilly",
+  "email": "r@test.com"
+}'
+"Error: The User does not have a post method implemented to handle HTTP method POST"%
+```
+
+### Thoughts
+
+We have an insane DX win if we integrate TS to get autocomplete for tables
+
+`npx harperdb studio` AND BOOM you have localhost:3000/studio up and running to interact with db
+
+In the studio I'd love to be able to change values in the input/cell instead of it doing a json representation that doesn't show the attributes
--- resources.js
+++ resources.js
@@ -1,22 +1,97 @@
-/** Here we can define any JavaScript-based resources and extensions to tables
+/* 
+	A few notes - 
+	1. This is insanely bare bones - there's no routing, no auth, no validation, no error handling, no testing, no validation. A normal production app would have all of these things and more
+	2. We're not exposing the retrieval of messages via ws bc normal react apps would fetch the messages via SSR or in a loader type thing (a la remix)
+	3. also we're not sending messages every time the ws connects bc ws connections drop and reconnect fairly often (wasteful)
+	4. There's no auth bc this is a super simple poc - in a real app we'd derive the user id / validate using jwt or cookie
+	5. I initally had a separate Chat table but decided to keep it extremely simple. The separate table would have allowed individual chat rooms on a per major city area basis. Happy to chat about how to adjust it to make that happen :D
+	
+*/
 
-export class MyCustomResource extends tables.TableName {
-	// we can define our own custom POST handler
-	post(content) {
-		// do something with the incoming content;
-		return super.post(content);
-	}
-	// or custom GET handler
-	get() {
-		// we can modify this resource before returning
-		return super.get();
-	}
+import dotenv from 'dotenv';
+dotenv.config();
+
+export class User extends tables.User {
+  // we can define our own custom POST handler
+  post(content) {
+    // do something with the incoming content;
+    return super.post(content);
+  }
+  // or custom GET handler
+  get() {
+    // we can modify this resource before returning
+    return super.get();
+  }
 }
+
+export class Weather extends Resource {
+  async get(searchParams, ...rest) {
+    const id = searchParams.get('id');
+    if (!id) {
+      throw new Error('Missing id');
+    }
+    const user = await tables.User.get(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const weather = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${user.location}&appid=${process.env.WEATHER_API_KEY}`
+    );
+    const json = await weather.json();
+
+    return { res: json };
+  }
+}
+
+/* WS client usage example (again very barebones):
+
+let ws = new WebSocket('ws://localhost:9926/Chat');
+
+ ws.send(JSON.stringify({
+  content: "WOW",
+  senderId: "user-123" 
+}))
  */
-// we can also define a custom resource without a specific table
-export class Greeting extends Resource {
-	// a "Hello, world!" handler
-	get() {
-		return { greeting: 'Hello, world!' };
-	}
+
+export class Chat extends Resource {
+  async connect(incoming, ...rest) {
+    const outgoingMessages = super.connect();
+
+    if (incoming) {
+      incoming.on('data', async (data) => {
+        // really interesting... we get the raw stream when it's a raw string i.e.
+        //data { contentType: 'application/octet-stream', data: <Buffer 57 4f 57> }
+
+        // otherwise if I send a stringified obj I get what appears to be the parsed
+
+        try {
+          // the ws is sending:
+          // content: content,
+          // senderId: 'user-123'
+          // but in a real app we'd have auth and use that id instead of allowing the client to pass whatever they want
+          const newMessage = {
+            ...data,
+            timestamp: new Date().toISOString(),
+          };
+
+          await tables.Message.operation(
+            {
+              operation: 'insert',
+              records: [newMessage],
+            },
+            false
+          );
+
+          outgoingMessages.send(JSON.stringify(newMessage));
+        } catch (error) {
+          console.log('error:', error);
+          outgoingMessages.send(
+            JSON.stringify({ type: 'error', message: error.message })
+          );
+        }
+      });
+    }
+    return outgoingMessages;
+  }
 }
